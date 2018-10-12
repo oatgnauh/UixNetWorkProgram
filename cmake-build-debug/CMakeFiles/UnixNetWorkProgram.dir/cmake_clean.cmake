25a40de94a4436c96125da3f78defbf89aba166d
pyhunter <892145616@qq.com>
2018-10-12 16:43:22 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/UnixNetWorkProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnixNetWorkProgram.dir/main.c.o"
+  "UnixNetWorkProgram.pdb"
+  "UnixNetWorkProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UnixNetWorkProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
